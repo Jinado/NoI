df82df26670d1424281cdfa99248c5cdf0d4f7ee
Johannes Emmoth <johannes.emmoth@gmail.com>
2020-10-15 22:51:34 +0200

    Finished bot

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-import { Client } from 'discord.js';
-const client = new Client();
+// Middleware
+require('dotenv').config()
+const Discord = require('discord.js');
+const Client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+Client.on("ready", () => {
+    console.log(`Logged in as ${Client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+Client.on("message", async msg => {
+    if(msg.author.id !== "766373529858998282")
+    {
+        let editedMsg = "";
+
+        for(let i = 0; i < msg.content.length; i++){
+            if(msg.content[i] == "i" || msg.content[i] == "I"){
+                editedMsg += "[REDACTED]";
+            } else {
+                editedMsg += msg.content[i];
+            }
+        }
+    
+        console.log(editedMsg);
+    
+        if(editedMsg.length !== msg.content.length){ // If the lengths are diffrent, all I:s have been replaced with [REDACTED]
+            await msg.delete().then(async msg => {
+                await msg.channel.send(`*${msg.author.username} said:* ${editedMsg}`).catch(console.error);
+            }).catch(console.error);
+        }
     }
 });
 
-client.login(process.env.NOI_BOT_TOKEN);
+Client.login(process.env.NOI_BOT_TOKEN);
